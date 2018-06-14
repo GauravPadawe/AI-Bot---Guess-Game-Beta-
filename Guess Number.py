13abc7ca8ed75aecf31ec3a5c1ca324a29148c54
Gaurav Padawe <39410780+GauravPadawe@users.noreply.github.com>
2018-06-15 00:18:39 +0530

    Update Guess Number.py

--- Guess Number.py
+++ Guess Number.py
@@ -1,29 +1,30 @@
 ﻿import random
 import sys
 
-def guess(a):
+def guess(a):                             # def guess function
     x = [1,2,3,4,5,6,7,8,9]
-    while True:
-        cpu = random.choice(x)
-        if a < cpu:
+    while True:                           # while loop to keep running again
+        cpu = random.choice(x)            # cpu will choose random number from list
+        if a < cpu:                       # if value of a < cpu
             return "Less"
-        elif a > cpu:
+        elif a > cpu:                     # else if a > cpu
             return "Greater"
-        elif a == cpu:
+        elif a == cpu:                    # else if a == cpu
             return "Equal"
             sys.exit()
         
-def again():
+def again():                                # Defining another function
     y = [1,2,3,4,5,6,7,8,9]
-    while True:
-        try:
+    while True:                             # while
+        try:                                                          # try if user input integer or string
             user = int(input("What's your Choice ?"))
-            if user not in y:
-                return "Out of context, just choose between 1 to 9"
-            else:
+            if user not in y:                                         # if user value is not in list Y
+                return "Out of context, just choose between 1 to 9"   # print out of context
+            else:                                                     # else run functio guess with intialised value
                 print (guess(user))
-        except:
+        except:                                                       # if string then return Invalid Input
             return "Invalid Input, try again"
-            continue
     
 print (guess(9))
+
+# CODED BY - GAURAV PADAWE
