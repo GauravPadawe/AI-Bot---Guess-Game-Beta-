a53b8023fb888c31faf4efd0da6a14257cb125e2
Gaurav Padawe <grvpdw92@gmail.com>
2018-06-15 00:10:33 +0530

    Initial commit

---
+++ Guess Number.py
@@ -0,0 +1,29 @@
+﻿import random
+import sys
+
+def guess(a):
+    x = [1,2,3,4,5,6,7,8,9]
+    while True:
+        cpu = random.choice(x)
+        if a < cpu:
+            return "Less"
+        elif a > cpu:
+            return "Greater"
+        elif a == cpu:
+            return "Equal"
+            sys.exit()
+        
+def again():
+    y = [1,2,3,4,5,6,7,8,9]
+    while True:
+        try:
+            user = int(input("What's your Choice ?"))
+            if user not in y:
+                return "Out of context, just choose between 1 to 9"
+            else:
+                print (guess(user))
+        except:
+            return "Invalid Input, try again"
+            continue
+    
+print (guess(9))
---
+++ README.md
@@ -0,0 +1,1 @@
+# AI-Bot - Guess-Game-Beta
